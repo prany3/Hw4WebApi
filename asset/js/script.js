d41a620056381d69c52f772a0c9b3e04853fbc49
Prany Davey <prany20@yahoo.com>
2021-08-18 15:10:32 -0500

    Timer updated and working

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,11 +1,45 @@
 //Timer
-let cTime = 120;
-function myTimer(){
-  if(cTime == 0){
-    document.getElementById("timerButton").innerHTML = "Start";
-  } else{
-    document.getElementById("timerButton").innerHTML = cTime;
-    cTime = cTime - 1;
-    setTimeout(myTimer, 1000);
-  }
+document.addEventListener('DOMContentLoaded', () => {
+
+})    
+const timeLeftDisplay = document.getElementById('time-left')
+console.log(timeLeftDisplay)
+const startBtn = document.querySelector('#start-button')
+const decreaseBtn = document.querySelector('#decrease-button')
+var timerInterval 
+var timeLeft = 360
+
+
+var questions =[
+    {question:"what is 1+1", answers:[1,2,3,4], correctAnswer: 2},
+    {question:"what is 1+1", answers:[1,2,3,4], correctAnswer: 2},
+    {question:"what is 1+1", answers:[1,2,3,4], correctAnswer: 2}
+
+]
+
+
+
+
+
+
+
+
+
+
+function countDown() {
+   timerInterval = setInterval(function() {
+        console.log(timeLeft)
+        if(timeLeft <= 0 ) {
+            clearInterval(timerInterval)
+        }
+        timeLeftDisplay.innerHTML = timeLeft
+        timeLeft -=1
+        
+    }, 1000);
 }
+function decreaseCount() {
+    timeLeft -=5    
+    timeLeftDisplay.innerHTML = timeLeft
+}
+startBtn.addEventListener('click', countDown)            
+decreaseBtn.addEventListener('click', decreaseCount)            
