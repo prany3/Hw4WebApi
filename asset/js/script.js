e3ef5e3be2ad76cd28760bd9340aeffbdd37e4fd
Prany <77269d@gmail.com>
2021-09-03 02:48:16 -0500

    QuestionsAndAnswersAdded

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,5 @@
 //Timer
 document.addEventListener('DOMContentLoaded', () => {
-
 })    
 const timeLeftDisplay = document.getElementById('time-left')
 console.log(timeLeftDisplay)
@@ -9,15 +8,91 @@ const decreaseBtn = document.querySelector('#decrease-button')
 var timerInterval 
 var timeLeft = 360
 
-
+//coding questions, answers, correctAnswers
 var questions =[
-    {question:"what is 1+1", answers:[1,2,3,4], correctAnswer: 2},
-    {question:"what is 1+1", answers:[1,2,3,4], correctAnswer: 2},
-    {question:"what is 1+1", answers:[1,2,3,4], correctAnswer: 2}
+    {
+        question: "What is the purpose of html?",
+        answers:{
+            a: styling of page,
+            b: interaction of page,
+            c: ,
+            d: framework of page,
+        },
+         correctAnswer: "d"
+    },
+    {
+        question: "What is the purpose of css?",
+        answers:{
+            a: 1,
+            b: 2,
+            c: styling the page,
+            d 4,
+        },
+         correctAnswer: "c"
+    },
+    {
+        question: "What is the purpose of javascript?",
+        answers:{
+            a: 1,
+            b: to make page interactive,
+            c: 3,
+            d: 4,
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which program uses semantic tags?",
+        answers:{
+            a: html,
+            b: javascript,
+            c: ,
+            d: css,
+        }, 
+        correctAnswer: "a"
+    },
+    {
+        question: "Which function is used to push data at the end of an array?",
+         answers:{
+            a: push(),
+            b: ,
+            c: 3,
+            d: 4,
+         },
+         correctAnswer: "a"
+    },
+    {
+        question: "How would you remove the end of an array?", 
+        answers:{
+            a: 1,
+            b: console.log(),
+            c: shift(),
+            d: pop(),
+        }, 
+        correctAnswer: "d"
+    },
+];
 
-]
 
 
+ 
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		// code will go here
+	}
+
+	function showResults(questions, quizContainer, resultsContainer){
+		// code will go here
+	}
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
 
 
 
